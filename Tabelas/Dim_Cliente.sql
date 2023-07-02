11605e13581ea350b7c28287be655fdd1f2d47ce
andre <andre@ANDRE>
2023-07-01 23:10:06 -0300

    Adicionar arquivos de projeto.

---
+++ Tabelas/Dim_Cliente.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Dim_Cliente]
+(
+    [Cod_Cliente] NVARCHAR(50) NOT NULL, 
+    [Desc_Cliente] NVARCHAR(200) NULL, 
+    [Cod_Cidade] NVARCHAR(50) NULL, 
+    [Desc_Cidade] NVARCHAR(200) NULL, 
+    [Cod_Estado] NVARCHAR(50) NULL, 
+    [Desc_Estado] NVARCHAR(200) NULL, 
+    [Cod_Regiao] NVARCHAR(50) NULL, 
+    [Desc_Regiao] NVARCHAR(200) NULL, 
+    [Cod_Segmento] NVARCHAR(50) NULL, 
+    [Desc_Segmento] NVARCHAR(200) NULL, 
+    CONSTRAINT [PK_Dim_Clientes] PRIMARY KEY ([Cod_Cliente])
+)
+WITH
+(
+    DISTRIBUTION = HASH (col1),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
